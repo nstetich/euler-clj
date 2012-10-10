f053c42d34549fd5afe564ad5e49f63eeb1f2eb9
Nick Stetich <nick@stetich.com>
2012-10-10 08:27:10 -0700

    Added optimization.

--- p7.clj
+++ p7.clj
@@ -4,25 +4,22 @@
   (zero? (mod m n)))
 
 (defn smallest-factor-of [n]
-  (first (for [x (range 2 n)
-               :when (factor-of? n x)
-               :while (<= x (quot n x))] x)))
-
-(defn factors-of
-  ([n] (factors-of n []))
-  ([n factors]
-     ;; (println (str "(factors-of " n " " factors ")"))
-     (if-let [fact (smallest-factor-of n)]
-       (let [compl (quot n fact)]
-         (recur compl (cons fact factors)))
-       (if (> n 1) (cons n factors) factors))))
+ (first (filter (partial factor-of? n)
+                (range 2 (inc (bigint (java.lang.Math/sqrt n)))))))
 
 (defn prime? [n]
-  (= (first (factors-of n)) n))
+  (nil? (smallest-factor-of n)))
 
 (def naturals (iterate inc 1))
 
 (def primes (filter prime? naturals))
 
-(println
- (nth primes 10000))
+(println (nth primes 10000))
+
+;; user=> (time (nth primes 10000))
+;; "Elapsed time: 81167.336 msecs"
+;; 104743
+
+;; user=> (time (nth primes 10000))
+;; "Elapsed time: 1308.829 msecs"
+;; 104729
