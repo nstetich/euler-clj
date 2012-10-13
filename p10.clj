17b7350a499ad37688458a7377e232363694033a
Nick Stetich <nick@stetich.com>
2012-10-13 16:35:41 -0700

    Added problem description.

--- p10.clj
+++ p10.clj
@@ -1,5 +1,8 @@
 (ns euler.p10a)
 
+;; The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+;; Find the sum of all the primes below two million.
+
 (defn multiple-of-any? [ns m]
   (let [max-n (long (java.lang.Math/sqrt m))]
     (some zero? (for [n ns :while (<= n max-n)] (mod m n)))))
